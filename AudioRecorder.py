47de7b6635d18fb836de199b11fac8520887324e
HHedima <HHedima@guilford.edu>
2025-12-04 00:18:35 -0500

    initial commit

---
+++ AudioRecorder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# /home/pi/Documents/Audio/AudioRecorder.py
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.mainloop()
